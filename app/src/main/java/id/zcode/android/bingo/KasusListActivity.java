15e49cb66c2e8e772f8da2f0f6188e02640e2341
Windu Purnomo <wpwindupurnomo@gmail.com>
2019-10-29 16:20:29 +0700

    form kasus

---
+++ app/src/main/java/id/zcode/android/bingo/KasusListActivity.java
@@ -0,0 +1,13 @@
+package id.zcode.android.bingo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KasusListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kasus_list);
+    }
+}

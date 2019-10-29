426445c70f87c14ab22a1995cd9325ffba41e5be
Windu Purnomo <wpwindupurnomo@gmail.com>
2019-10-29 13:13:31 +0700

    initial commit

---
+++ app/src/main/java/id/zcode/android/bingo/MainActivity.java
@@ -0,0 +1,29 @@
+package id.zcode.android.bingo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLogin(View view) {
+        Toast.makeText(this, "Klik button login", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onForgot(View view) {
+        Intent intent = new Intent(this, ForgotPasswordActivity.class);
+        startActivity(intent);
+    }
+
+    public void onRegister(View view) {
+        Toast.makeText(this, "Klik text buat akun baru", Toast.LENGTH_SHORT).show();
+    }
+}
